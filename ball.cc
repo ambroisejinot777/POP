b2ac9236d183882174ec0d561947e5e8ae96a6c1
Ambroise Jinot <ambroise.jinot@epfl.ch>
2026-03-08 12:07:31 +0100

    modif ball1 ambroise

--- ball.cc
+++ ball.cc
@@ -9,3 +9,6 @@ void Change_delta();
 
 bool Check_invalid_delta();
 bool Check_ball_outside();
+bool Check_collision_balls();
+bool Check_collision_ball_brick();
+bool Check_collision_paddle_ball();
