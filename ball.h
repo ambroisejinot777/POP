241131fd6f17e66f831c2913ceb4447228641118
Ambroise Jinot <ambroise.jinot@epfl.ch>
2026-03-13 09:34:53 +0100

    continue changes

--- ball.h
+++ ball.h
@@ -6,83 +6,16 @@
 
 using namespace std;
 
-class Point {
-    public : 
-        Point ()
-        : center_x(0.0), center_y(0.0), radius(0.0), delta_x(0.0), delta_y(0.0)
-        {}
-        Point (double x, double y, double r, double dx, double dy)
-        {
-          if (x >= 0 && x <= arena_size) {
-            center_x = x;
-          }
-          else {
-            cout << message::ball_outside(x, y) << endl;
-          }
-          if (y >= 0 && y <= arena_size) {
-            center_y = y;
-          }
-          else {
-            cout << message::ball_outside(x, y) << endl;
-          }
-          if (r == 1.)  {
-            radius = r;
-          }
-          else {
-            cout << "Rayon de balle mauvais" << endl;
-          }
-          delta_x = dx;  
-          delta_y = dy;
-        }
-        Point (double x=0.0, double y=0.0, double r=0.0, double dx=0.0, double dy=0.0)
-        : center_x(x), center_y(y), radius(r), delta_x(dx), delta_y(dy)
-        {
-            if (x >= 0 && x <= 1) {
-                center_x = x;
-            }
-            else {
-                cout << message::ball_outside(x, y) << endl;
-            }
-            if (y >= 0 && y <= arena_size) {
-                center_y = y;
-            }
-            else {
-                cout << message::ball_outside(x, y) << endl;
-            }
-            if (r == 1.)  {
-                radius = r;
-            }
-            else {
-                cout << "Rayon de balle mauvais" << endl;
-            }
-            delta_x = dx;  
-            delta_y = dy;
-        }
-        Point(Point const& old_point)
-        : center_x(old_point.center_x), center_y(old_point.center_y), radius(old_point.radius), delta_x(old_point.delta_x), delta_y(old_point.delta_y)
-        {}
-        void change_position();
-        void change_delta();
-
-    private :
-        Point Ball_center;
-        Point Ball_delta;
-        double Ball_radius;
-        
-};
 class Ball {
     public :
         Ball ()
-        : associated_point()
-        {}
-        Ball (double x, double y, double r, double dx, double dy)
-        : associated_point(x, y, r, dx, dy)
+        : 
         {}
         Ball (double x=0.0, double y=0.0, double r=0.0, double dx=0.0, double dy=0.0)
-        : associated_point(x, y, r, dx, dy)
+        : Ball_center(x, y), Ball_delta(dx, dy), Ball_radius(r)
         {}
         Ball(Ball const& old_ball)
-        : associated_point(old_ball.associated_point)
+        : Ball_center(old_ball.Ball_center), Ball_delta(old_ball.Ball_delta), Ball_radius(old_ball.Ball_radius)
         {}
         bool check_invalid_delta();
         bool check_ball_outside();
