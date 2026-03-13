1aabbe0c282db642b07c04e7ab65c3e5b975cbfd
Ralph Gustave Honhon <honhon@ic1-ubt22-5019.intranet.epfl.ch>
2026-03-13 15:22:08 +0100

    started brick checks

--- brick.cc
+++ brick.cc
@@ -1,10 +1,26 @@
 #include "brick.h"
 
-
 // CONSTRUCTORS
 
 Brick::Brick(double x = 0.0, double y = 0.0, double width = 0.0, int hit_points = 0, Brick_type type = DEFAULT)
-    : square(x, y, width), hit_points(hit_points), type(type) {}
+    : square(x, y, width), hit_points(hit_points), type(type)
+{
+    if (width < brick_size_min)
+    {
+        error(message::invalid_brick_size(width));
+    }
+    if ((x + width / 2 > arena_size) or
+        (x - width / 2 < 0) or
+        (y + width / 2 > arena_size) or
+        (y - width / 2 < 0))
+    {
+        error(message::brick_outside(x, y));
+    }
+    if (hit_points > 0 and hit_points < 8)
+    {
+        error(message::invalid_hit_points(hit_points));
+    }
+}
 
 Brick::Brick(Brick const &old_brick)
     : square(old_brick.square), hit_points(old_brick.hit_points), type(old_brick.type) {}
