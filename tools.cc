82c779dcba3f825a6fc2a3f520ec73369c9bb1e8
Ralph Gustave Honhon <honhon@ic1-ubt22-5019.intranet.epfl.ch>
2026-03-13 13:16:38 +0100

    corrected tools cnostructor

--- tools.cc
+++ tools.cc
@@ -1,5 +1,38 @@
 #include "tools.h"
 
+
+// UTILITY CLASSES
+
+// POINT
+
+Point::Point (double x=0.0, double y=0.0)
+    :center_x(x), center_y(y){} 
+    
+Point::Point(Point const& old_point)
+    :center_x(old_point.center_x), center_y(old_point.center_y){}
+
+// SQUARE
+
+Square::Square (double x=0.0, double y=0.0, double w=0.0, double h=0.0)
+    : square_center(x, y), width(w), height(h){} 
+
+Square::Square (Square const& old_square)
+    : square_center(old_square.square_center), width(old_square.width), height(old_square.height){}
+
+// CIRCLE
+
+Circle::Circle(double x=0.0, double y=0.0, double r=0.0)
+    :circle_center(x, y), radius(r){} 
+
+Circle::Circle (Circle const& old_circle)
+    :circle_center(old_circle.circle_center), radius(old_circle.radius){}
+
+
+
+
+
+
+
 int collision_type() {
 
 }
--- tools.h
+++ tools.h
@@ -4,12 +4,9 @@
 
 class Point {
     public : 
-    Point (double x=0.0, double y=0.0)
-        : center_x(x), center_y(y)
-        {} 
-    Point(Point const& old_point)
-        : center_x(old_point.center_x), center_y(old_point.center_y)
-        {}
+    Point (double x=0.0, double y=0.0);
+
+    Point(Point const& old_point);
     private :
         double center_x;
         double center_y;
@@ -17,12 +14,8 @@ class Point {
 
 class Square {
     public : 
-    Square (double x=0.0, double y=0.0, double w=0.0, double h=0.0)
-        : square_center(x, y), width(w), height(h)
-        {} 
-    Square (Square const& old_square)
-        : square_center(old_square.square_center), width(old_square.width), height(old_square.height)
-        {}
+    Square (double x=0.0, double y=0.0, double w=0.0, double h=0.0);
+    Square (Square const& old_square);
         
     private :
         Point square_center;
@@ -32,13 +25,9 @@ class Square {
 
 class Circle {
     public : 
-    Circle (double x=0.0, double y=0.0, double r=0.0)
-        : circle_center(x, y), radius(r)
-        {} 
-    Circle (Circle const& old_circle)
-        : circle_center(old_circle.circle_center), radius(old_circle.radius)
-        {}
-        
+        Circle (double x=0.0, double y=0.0, double r=0.0);
+        Circle (Circle const& old_circle);
+
     private :
         Point circle_center;
         double radius;
@@ -48,12 +37,6 @@ bool circle_circle_intersection(Circle const& c1, Circle const& c2);
 bool circle_square_intersection(Circle const& c, Square const& s);
 bool square_square_intersection(Square const& s1, Square const& s2);
 
-int collision_type() ;
-void vertical_wall_collision();
-void horizontal_wall_collision();
-void brick_collision();
-void paddle_collision();
-void ball_collision();
 
 #endif
 
