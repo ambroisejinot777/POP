9eebfa996aa2b641863498abfe74ca8996067f4e
Ambroise Jinot <ambroise.jinot@epfl.ch>
2026-03-13 13:19:42 +0100

    correction paddle et ball propre

--- ball.cc
+++ ball.cc
@@ -5,12 +5,21 @@
 
 using namespace std;
 
+// CONSTRUCTEURS DE BALL
+
 Ball::Ball(double x=0.0, double y=0.0, double r=0.0, double dx=0.0, double dy=0.0)
         : ball_center(x, y), ball_delta(dx, dy), ball_radius(r)
         {}
+
 Ball::Ball(Ball const& old_ball)
         : ball_center(old_ball.ball_center), ball_delta(old_ball.ball_delta), ball_radius(old_ball.ball_radius)
         {}
+
+// FONCTIONS DE VERIFICATION DE SAISIE POUR BALL
+
+
+
+// FONCTIONS D'ACTIONS DE BALL
 
 void change_position() {
         // code to change the position of the ball based on its current delta
--- paddle.cc
+++ paddle.cc
@@ -4,9 +4,15 @@
 
 using namespace std;
 
+// CONSTRUCTEURS DE PADDLE
+
 Paddle::Paddle (double x=0.0, double y=0.0, double r=0.0)
         : paddle_center(x, y), radius(r)
         {}
+
 Paddle::Paddle (Paddle const& old_paddle)
         : paddle_center(old_paddle.paddle_center), radius(old_paddle.radius)
         {}
+
+// FONCTIONS DE VERIFICATION DE SAISIE POUR PADDLE
+
