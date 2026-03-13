b9c510fa7dc9329617d2c9d606cf11367d10d4fa
Ambroise Jinot <ambroise.jinot@epfl.ch>
2026-03-13 09:39:29 +0100

    continue changes

--- tools.h
+++ tools.h
@@ -5,55 +5,13 @@
 class Point {
     public : 
     Point ()
-        : center_x(0.0), center_y(0.0), radius(0.0), delta_x(0.0), delta_y(0.0)
+        : center_x(0.0), center_y(0.0)
         {}
-        Point (double x, double y, double r, double dx, double dy)
-        {
-          if (x >= 0 && x <= arena_size) {
-            center_x = x;
-          }
-          else {
-            cout << message::ball_outside(x, y) << endl;
-          }
-          if (y >= 0 && y <= arena_size) {
-            center_y = y;
-          }
-          else {
-            cout << message::ball_outside(x, y) << endl;
-          }
-          if (r == 1.)  {
-            radius = r;
-          }
-          else {
-            cout << "Rayon de balle mauvais" << endl;
-          }
-          delta_x = dx;  
-          delta_y = dy;
-        }
-    Point (double x=0.0, double y=0.0, double r=0.0, double dx=0.0, double dy=0.0)
-        : center_x(x), center_y(y), radius(r), delta_x(dx), delta_y(dy)
-        {
-            if (x >= 0 && x <= 1) {
-                center_x = x;
-            }
-            else {
-                cout << message::ball_outside(x, y) << endl;
-            }
-            if (y >= 0 && y <= arena_size) {
-                center_y = y;
-            }
-            else {
-                cout << message::ball_outside(x, y) << endl;
-            }
-            if (r == 1.)  {
-                radius = r;
-            }
-            else {
-                cout << "Rayon de balle mauvais" << endl;
-            }
-            delta_x = dx;  
-            delta_y = dy;
-        }
+        Point (double x, double y)
+        {}
+    Point (double x=0.0, double y=0.0)
+        : center_x(x), center_y(y)
+        {} 
     Point(Point const& old_point)
         : center_x(old_point.center_x), center_y(old_point.center_y), radius(old_point.radius), delta_x(old_point.delta_x), delta_y(old_point.delta_y)
         {}
