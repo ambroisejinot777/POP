958821ba98f4ed567c5d2f221b69809fef2df60f
Ralph Gustave Honhon <honhon@ic1-ubt22-5019.intranet.epfl.ch>
2026-03-13 15:48:51 +0100

    finished brick checking process

--- brick.cc
+++ brick.cc
@@ -2,13 +2,22 @@
 
 // CONSTRUCTORS
 
-Brick::Brick(double x = 0.0, double y = 0.0, double width = 0.0, int hit_points = 0, Brick_type type = DEFAULT)
+Brick::Brick(double x = 0.0, double y = 0.0, double width = 0.0, int hit_points = 0, int type = 0)
     : square(x, y, width), hit_points(hit_points), type(type)
 {
-    if (width < brick_size_min)
-    {
-        error(message::invalid_brick_size(width));
-    }
+    check_brick_position(x, y);
+    check_brick_size(width);
+    check_brick_hit_points(hit_points);
+    check_brick_type(type);
+}
+
+Brick::Brick(Brick const &old_brick)
+    : square(old_brick.square), hit_points(old_brick.hit_points), type(old_brick.type) {}
+
+// CHECKING FUNCTIONS
+
+void check_brick_position(double x, double y)
+{
     if ((x + width / 2 > arena_size) or
         (x - width / 2 < 0) or
         (y + width / 2 > arena_size) or
@@ -16,11 +25,24 @@ Brick::Brick(double x = 0.0, double y = 0.0, double width = 0.0, int hit_points 
     {
         error(message::brick_outside(x, y));
     }
-    if (hit_points > 0 and hit_points < 8)
+}
+
+void check_brick_size(double width)
+{
+    if (width < brick_size_min)
+        error(message::invalid_brick_size(width));
+}
+
+void check_brick_hit_points(int hit_points)
+{
+    if (hit_points < 1 and hit_points > 7)
     {
         error(message::invalid_hit_points(hit_points));
     }
 }
 
-Brick::Brick(Brick const &old_brick)
-    : square(old_brick.square), hit_points(old_brick.hit_points), type(old_brick.type) {}
+void check_brick_type(int type)
+{
+    if (type < 0 or type > 3)
+        error(message::invalid_brick_type(type));
+}
--- brick.h
+++ brick.h
@@ -4,24 +4,26 @@
 #include "message.h"
 #include "tools.h"
 
-enum Brick_type
-{
-    DEFAULT,
-    RAINBOW_BRICK,
-    BALL_BRICK
-};
+// BRICK CLASS
 
 class Brick
 {
 
 public:
-    Brick(double x, double y, double width, int hit_points, Brick_type type);
+    Brick(double x, double y, double width, int hit_points, int type);
     Brick(Brick const &old_brick);
 
 private:
     Square square;
     int hit_points;
-    Brick_type type;
+    int type;
 };
+
+// CHECKING FUNCTIONS
+
+void check_brick_position(double x, double y);
+void check_brick_size(double width);
+void check_brick_hit_points(int hit_points);
+void check_brick_type(int type);
 
 #endif
