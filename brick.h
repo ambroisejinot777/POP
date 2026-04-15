000515b392c4fbea80dc023d9eca04a55b87b8c4
Ambroise Jinot <ambroise.jinot@epfl.ch>
2026-04-15 16:01:29 +0200

    creation heritage brick

--- brick.h
+++ brick.h
@@ -3,16 +3,17 @@
 
 #include "message.h"
 #include "tools.h"
+#include "graphic.h"
 
-// BRICK CLASS
+BRICK CLASS
 
 class Brick
 {
 
 public:
-    Brick(bool& error_occured, double x = 0.0, double y = 0.0, double width = 0.0,
+    Brick(double x = 0.0, double y = 0.0, double width = 0.0,
          int hit_points = 0, int type = 0);
-    // Brick(Brick const &old_brick) = 0;
+    Brick(Brick const &old_brick);
     double get_x() const;
     double get_y() const;
     Square get_square() const;
@@ -28,9 +29,10 @@ typedef vector<unique_ptr<Brick>>  Brick_list;
 
 // CHECKING FUNCTIONS
 
-void check_brick_position(double x, double y, double width, bool& error_occured);
-void check_brick_size(double width, bool& error_occured);
-void check_brick_hit_points(int hit_points, bool& error_occured);
-void check_brick_type(int type, bool& error_occured);
+void check_brick_position(double x, double y, double width);
+void check_brick_size(double width);
+void check_brick_hit_points(int hit_points);
+void check_brick_type(int type);
 
 #endif
+
